0169abf5edc154a55cff31efd64d72dd7608512c
Ivan <ivan85mk@yahoo.com>
2023-10-05 12:00:28 +0200

    Add parameters to init script, show usage and example if wrong

--- examples/init_repo.sh
+++ examples/init_repo.sh
@@ -1,11 +1,17 @@
+if [ -z $1 ] || [ -z $2 ]
+then
+  echo "Init script takes 'name' and 'email' parameters, in that order"
+  echo "Example: bash init_repo Solomon solomon@notnow.later"
+  exit 1
+fi
 alias ll="ls -l"
 echo "Going to 'home' directory"
 cd /home
 echo "Setting global Git config"
-git config --global --unset user.email "email"
-git config --global --add user.email "email"
-git config --global --unset user.name "name"
-git config --global --add user.name "name"
+git config --global --unset user.name
+git config --global --add user.name $1
+git config --global --unset user.email
+git config --global --add user.email $2
 echo
 git clone https://github.com/SuperPranx/udemy-docker.git
 echo
