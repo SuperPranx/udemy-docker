c14054a92cca1769c310aaf58420c985bfdacf5b
Ivan <ivan85mk@yahoo.com>
2023-10-05 11:16:01 +0200

    Adjust init script to set global git user because no local repo exists at that time

--- examples/init_repo.sh
+++ examples/init_repo.sh
@@ -1,5 +1,13 @@
+alias ll="ls -l"
+echo "Going to 'home' directory"
 cd /home
-git config user.email "email"
-git config user.name "name"
+echo "Setting global Git config"
+git config --global --unset user.email "email"
+git config --global --add user.email "email"
+git config --global --unset user.name "name"
+git config --global --add user.name "name"
+echo
 git clone https://github.com/SuperPranx/udemy-docker.git
+echo
 cd udemy-docker
+ls -l
